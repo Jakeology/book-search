b93fdaa98162755bb0b2f55136dc6560c54734ed
Jake <xxjakexx14@gmail.com>
2022-01-23 20:46:48 -0500

    added mutations and auth to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,14 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
